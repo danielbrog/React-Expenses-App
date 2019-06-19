dec98a1942c637a798d1789a017bde595e36990f
danielbrog <brog.daniel@gmail.com>
2019-06-19 10:15:02 -0400

    removed dummy data

--- src/app.js
+++ src/app.js
@@ -7,39 +7,7 @@ import 'react-dates/lib/css/_datepicker.css'
 import AppRouter from './routers/AppRouter'
 import configureStore from './store/configureStore'
 
-import {addExpense} from './actions/expenses'
-import expenseSelector from './selectors/expenses'
-import {setTextFilter} from './actions/filters'
-
 const store = configureStore()
-
-
-
-store.dispatch(addExpense({
-    description: 'waterbill',
-    note: 'paid water bill',
-    amount:1300,
-    createdAt:1000
-}))
-store.dispatch(addExpense({
-    description: 'Gas Bill',
-    note: 'paid gas bill',
-    amount:700,
-    createdAt:10000000
-}))
-store.dispatch(addExpense({
-    description: 'Rent',
-    note: 'paid rent',
-    amount:8800,
-    createdAt:10000
-}))
-
-//console.log(expenseSelector(store.getState().expenses,store.getState().filters))
-
-//console.log(store.getState())
-//renders root class
-
-
 
 const jsx = (
     <Provider store={store}>
