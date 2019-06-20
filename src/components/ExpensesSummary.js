222d39754e1973bb42f303ea4dec2cbebdf7b660
danielbrog <brog.daniel@gmail.com>
2019-06-20 10:53:33 -0400

    added expenseSummary to Dashboard, minor visual updates on ExpensesSummary

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -2,14 +2,18 @@ import React from 'react'
 import {connect} from 'react-redux'
 import selectExpenses from '../selectors/expenses'
 import expensesTotal from '../selectors/expenses-total'
+import numeral from 'numeral'
 
-export const ExpensesSummary = (props) => (
+export const ExpensesSummary = (props) => {
+    const expenseWord = props.expenses.length < 2 ? 'expense':'expenses'
+    return (
     <div>
         {props.expenses.length !== 0 && 
-            (<p>Viewing {props.expenses.length} expenses totalling {expensesTotal(props.expenses)}</p>)
+            (<p>Viewing {props.expenses.length} {expenseWord} totalling {numeral(expensesTotal(props.expenses) / 100).format('$0,0.00')}</p>)
         }
     </div>
-)
+    )
+}
 
 const mapStateToProps = (state) => {
     return {
