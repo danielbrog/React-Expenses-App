9bc16ebb9b119c9202ddec43f94cb18e8e6fb0ab
danielbrog <brog.daniel@gmail.com>
2019-06-27 14:07:11 -0400

    changed NavLink text to reflect what it does properly

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import {NavLink} from 'react-router-dom'
 const Header = () => (
     <header>
         <h1>Expensify</h1>
-        <NavLink to="/ExpenseApp" activeClassName="is-active" exact={true}>Home</NavLink>
+        <NavLink to="/ExpenseApp" activeClassName="is-active" exact={true}>Expenses</NavLink>
         <NavLink to="/ExpenseApp/create" activeClassName="is-active">Create</NavLink>
     </header>
 )
